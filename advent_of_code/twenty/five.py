26145591192538ed42974c5d40f564a23aa2a2eb
Gurashish Singh <gurashish1singh@gmail.com>
2022-10-18 16:33:04 -0400

    Found another solution

--- advent_of_code/twenty/five.py
+++ advent_of_code/twenty/five.py
@@ -11,10 +11,8 @@ def main():
     # """
     sf = SeatFinder(INPUT_STRING_5)
     sf.find_seat()
-    valid_seats, invalid_seats = sf.valids, sf.invalids
-    if invalid_seats:
-        print(f"Found some invalid seat string:\n{invalid_seats = }")
-    highest_seat_id = max(valid_seats)
+    # sf.find_seat_more_efficiently()
+    highest_seat_id = max(sf.seats)
     print(f"{highest_seat_id = }")
 
 
@@ -22,37 +20,21 @@ class SeatFinder:
 
     COLUMNS = list(range(8))
     ROWS = list(range(128))
-    VALID_STRING_LENGTH = 10
 
     def __init__(self, input_string: str) -> None:
         self.input_string = input_string
-        self.invalids: list[str] = []
         self.seat_numbers = parse_input(self.input_string)
-        self.valids: list[str] = []
+        self.seats: list[int] = []
 
     def find_seat(self) -> None:
         for seat in self.seat_numbers:
             first_seven = seat[:7]
             last_three = seat[7:]
-            if self.validate_string(seat, first_seven, last_three):
-                row_number = self.traverse_rows(first_seven)
-                column_number = self.traverse_columns(last_three)
-                self.valids.append(
-                    self.unique_seat_id(row_number, column_number)
-                )
-
-    def validate_string(self, seat: str, first_seven: str, last_three: str) -> bool:
-        if len(seat) != self.VALID_STRING_LENGTH:
-            self.invalids.append(seat)
-            return False
-
-        if "F" not in first_seven and "B" not in first_seven:
-            self.invalids.append(seat)
-            return False
-        elif "R" not in last_three and "L" not in last_three:
-            self.invalids.append(seat)
-            return False
-        return True
+            row_number = self.traverse_rows(first_seven)
+            column_number = self.traverse_columns(last_three)
+            self.seats.append(
+                self.unique_seat_id(row_number, column_number)
+            )
 
     def traverse_rows(self, seat: str) -> int:
         rows_left_to_search = self.ROWS
@@ -76,3 +58,12 @@ class SeatFinder:
 
     def unique_seat_id(self, row_number: int, column_number: int) -> int:
         return (row_number * 8) + column_number
+
+    def find_seat_more_efficiently(self) -> None:
+        # This one is done through bit magic.
+        # Kudos @anthonywritescode for this solution.
+        for seat in self.seat_numbers:
+            seat = seat.replace("F", "0").replace("B", "1")
+            seat = seat.replace("L", "0").replace("R", "1")
+            # Converting to base 2
+            self.seats.append(int(seat, base=2))
